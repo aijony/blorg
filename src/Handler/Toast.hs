3f8c76e983807d308a25495b2b318a6d6ecdb941
Aidan Nyquist <contact@aidannyquist.com>
2018-01-22 14:42:29 -0600

    Add patch

---
+++ src/Handler/Toast.hs
@@ -0,0 +1,6 @@
+module Handler.Toast where
+
+import Import
+
+getToastR :: Handler Html
+getToastR = error "Not yet implemented: getToastR"
---
+++ test/Handler/ToastSpec.hs
@@ -0,0 +1,10 @@
+module Handler.ToastSpec (spec) where
+
+import TestImport
+
+spec :: Spec
+spec = withApp $ do
+
+    describe "getToastR" $ do
+        error "Spec not implemented: getToastR"
+
